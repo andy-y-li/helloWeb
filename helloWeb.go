e7ab301dc73755120e7847e90c2c9d7e050a8718
Andy <andy_y_li@163.com>
2017-10-20 17:46:56 +0800

    Add weather function

--- helloWeb.go
+++ helloWeb.go
@@ -1,13 +1,56 @@
 package main
 
-import "net/http"
+import (
+    "fmt"
+    "net/http"
+    "encoding/json"
+    "strings"
+)
+
+type weatherData struct {
+    Name string `json:"name"`
+    Main struct{
+        Kelvin float64 `json:"temp"`
+    } `json:"main"`
+}
 
 func main() {
-    http.HandleFunc("/", hello)
+    http.HandleFunc("/hello", hello)
+    http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
+        city := strings.SplitN(r.URL.Path, "/", 3)[2]
+        //fmt.Println("city:", city)
+        data, err := query(city)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        w.Header().Set("Content-Type", "application/json; charset=utf-8")
+        json.NewEncoder(w).Encode(data)
+    })
+
     http.ListenAndServe(":8080", nil)
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("hello!"))
+}
+
+func query(city string) (weatherData, error) {
+    qstr := "http://api.openweathermap.org/data/2.5/weather?APPID=YOUR_API_KEY&q=" + city
+    fmt.Println(qstr)
+    resp, err := http.Get(qstr)
+    if err != nil {
+        return weatherData{}, err
+    }
+
+    defer resp.Body.Close()
+
+    var d weatherData
+
+    if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+        return weatherData{}, err
+    }
+
+    return d, nil
 }
 
