b062596c79195fdc310569652bea184d34c2be6d
Andy <andy_y_li@163.com>
2017-10-30 23:01:47 +0800

    Fix a bug of getWeatherCode.

--- weather/weatherCode.go
+++ weather/weatherCode.go
@@ -1,98 +1,98 @@
 package weatherCode
 
 import (
-    "encoding/xml"
-    "io/ioutil"
-    "log"
-    //"strconv"
+	"encoding/xml"
+	"errors"
+	"io/ioutil"
+	"log"
+	//"strconv"
 )
 
 type StringResources struct {
-    XMLName xml.Name `xml:"China"`
-    ResourceString []ResourceString `xml:"province"`
+	XMLName        xml.Name         `xml:"China"`
+	ResourceString []ResourceString `xml:"province"`
 }
 
 type ResourceString struct {
-    XMLName xml.Name `xml:"province"`
-    StringID string `xml:"id,attr"`
-    StringName string `xml:"name,attr"`
-    CityList []City `xml:"city"`
-    //InnerText string `xml:",innerxml"`
+	XMLName    xml.Name `xml:"province"`
+	StringID   string   `xml:"id,attr"`
+	StringName string   `xml:"name,attr"`
+	CityList   []City   `xml:"city"`
+	//InnerText string `xml:",innerxml"`
 }
 
 type City struct {
-    CityName xml.Name `xml:"city"`
-    StringID string `xml:"id,attr"`
-    StringName string `xml:"name,attr"`
-    CountyList []CountyCode `xml:"county"`
+	CityName   xml.Name     `xml:"city"`
+	StringID   string       `xml:"id,attr"`
+	StringName string       `xml:"name,attr"`
+	CountyList []CountyCode `xml:"county"`
 }
 
 type CountyCode struct {
-    CountyName xml.Name `xml:"county"`
-    StringID string `xml:"id,attr"`
-    StringName string `xml:"name,attr"`
-    WeatherCode string `xml:"weatherCode,attr"`
+	CountyName  xml.Name `xml:"county"`
+	StringID    string   `xml:"id,attr"`
+	StringName  string   `xml:"name,attr"`
+	WeatherCode string   `xml:"weatherCode,attr"`
 }
 
 type Area struct {
-    ProvinceName string
-    CityName string
-    CountyName string
+	ProvinceName string
+	CityName     string
+	CountyName   string
 }
 
 func ReadCityCode() {
-    content, err := ioutil.ReadFile("cityCodes.xml")
-    if err != nil {
-        log.Fatal(err)
-    }
-    var  result StringResources
-    err = xml.Unmarshal(content, &result)
-    if err != nil {
-        log.Fatal(err)
-    }
-    //log.Println(result)
-    //log.Println(result.ResourceString)
-    for _, line := range result.ResourceString {
-        log.Println("province-id:" + line.StringID + " name:" + line.StringName)
-        for _, citys := range line.CityList {
-            log.Println("city-id:" + citys.StringID + " name:" + citys.StringName)
-            for _, counties := range citys.CountyList {
-                log.Println("county id:" + counties.StringID + " name:" + counties.StringName + " weatherCode:" + counties.WeatherCode)
-            }
+	content, err := ioutil.ReadFile("cityCodes.xml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var result StringResources
+	err = xml.Unmarshal(content, &result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//log.Println(result)
+	//log.Println(result.ResourceString)
+	for _, line := range result.ResourceString {
+		log.Println("province-id:" + line.StringID + " name:" + line.StringName)
+		for _, citys := range line.CityList {
+			log.Println("city-id:" + citys.StringID + " name:" + citys.StringName)
+			for _, counties := range citys.CountyList {
+				log.Println("county id:" + counties.StringID + " name:" + counties.StringName + " weatherCode:" + counties.WeatherCode)
+			}
 
-        }
-    }
+		}
+	}
 }
 
-func GetWeatherCode(where Area) string{
-    if where.CountyName == nil {
-        where.CountyName = where.CityName
-    }
-    content, err := ioutil.ReadFile("cityCodes.xml")
-    if err != nil {
-        log.Fatal(err)
-    }
-    var  result StringResources
-    err = xml.Unmarshal(content, &result)
-    if err != nil {
-        log.Fatal(err)
-    }
+func GetWeatherCode(where Area) (wCode string, err error) {
+	if where.CountyName == "" {
+		where.CountyName = where.CityName
+	}
+	content, err := ioutil.ReadFile("cityCodes.xml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var result StringResources
+	err = xml.Unmarshal(content, &result)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    for _, line := range result.ResourceString {
-        if line.StringName != where.ProvinceName {
-            continue
-        }
-        for _, citys := range line.CityList {
-            if citys.StringName != where.CityName {
-                continue
-            }
-            for _, counties := range citys.CountyList {
-                if counties.StringName == where.CountyName {
-                    return counties.WeatherCode
-                }
-            }
-        }
-    }
-    return nil
+	for _, line := range result.ResourceString {
+		if line.StringName != where.ProvinceName {
+			continue
+		}
+		for _, citys := range line.CityList {
+			if citys.StringName != where.CityName {
+				continue
+			}
+			for _, counties := range citys.CountyList {
+				if counties.StringName == where.CountyName {
+					return counties.WeatherCode, nil
+				}
+			}
+		}
+	}
+	return "", errors.New("Not Found!")
 }
-
--- weather/weatherCode_test.go
+++ weather/weatherCode_test.go
@@ -1,14 +1,20 @@
 package weatherCode
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestReadCityCodes(t *testing.T) {
-    ReadCityCode()
+	ReadCityCode()
 }
 
-func TestGetWeatherCode(t *testing) {
-    w Area := {"台湾", "台北", "桃园"}
-    wCode := GetWeatherCode(w)
-    log.Printf("%v:%s\n", w, wCode)
+func TestGetWeatherCode(t *testing.T) {
+	w := Area{"台湾", "台北", "桃园"}
+	wCode, err := GetWeatherCode(w)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%v, weatherCode:%s\n", w, wCode)
+	}
 }
-
